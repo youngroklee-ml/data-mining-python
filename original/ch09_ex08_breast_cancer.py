1f22402436bdf155f7d12c70a51f09542f858f88
Youngrok Lee <cakeil@gmail.com>
2026-01-06 01:29:36 -0500

    build two models separately

--- original/ch09_ex08_breast_cancer.py
+++ original/ch09_ex08_breast_cancer.py
@@ -24,19 +24,37 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_
 
 # train SVM with Gaussian kernel with sigma = 0.5
 # See [kernel function parameterization](https://scikit-learn.org/stable/modules/svm.html#svm-kernels)
-svm_model = SVC(
+svm_model1 = SVC(
   C=10, 
   kernel='rbf', 
-  gamma=0.5, # Please also try 2
+  gamma=0.5,
   tol=1e-5
   )
-svm_model.fit(X_train, y_train)
+svm_model1.fit(X_train, y_train)
 
 # confusion matrix on train data
-cm_train = confusion_matrix(y_train, svm_model.predict(X_train))
-print(cm_train.T)
+cm_train1 = confusion_matrix(y_train, svm_model1.predict(X_train))
+print(cm_train1.T)
 
 # confusion matrix on test data
-cm_test = confusion_matrix(y_test, svm_model.predict(X_test))
-print(cm_test.T)
+cm_test1 = confusion_matrix(y_test, svm_model1.predict(X_test))
+print(cm_test1.T)
 
+
+# train SVM with Gaussian kernel with sigma = 2
+# See [kernel function parameterization](https://scikit-learn.org/stable/modules/svm.html#svm-kernels)
+svm_model2 = SVC(
+  C=10, 
+  kernel='rbf', 
+  gamma=2,
+  tol=1e-5
+  )
+svm_model2.fit(X_train, y_train)
+
+# confusion matrix on train data
+cm_train2 = confusion_matrix(y_train, svm_model2.predict(X_train))
+print(cm_train2.T)
+
+# confusion matrix on test data
+cm_test2 = confusion_matrix(y_test, svm_model2.predict(X_test))
+print(cm_test2.T)
